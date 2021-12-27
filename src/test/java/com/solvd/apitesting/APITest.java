2e91162731186c58ab04e786ee0218e3f2bcf452
jovchinnikova <juliya.ovcha@gmail.com>
2021-12-27 20:13:46 +0300

    Verify repositories and verify created repository tests were created

--- src/test/java/com/solvd/apitesting/APITest.java
+++ src/test/java/com/solvd/apitesting/APITest.java
@@ -2,13 +2,32 @@ package com.solvd.apitesting;
 
 import org.testng.annotations.Test;
 
+import java.util.UUID;
+
 public class APITest {
 
     @Test
     public void verifyUser(){
         GetUserMethod getUserMethod = new GetUserMethod();
-        getUserMethod.setHeaders(String.format("Authorization=%s", "token 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"));
+        getUserMethod.setHeaders(String.format("Authorization=%s", "token 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"));
         getUserMethod.callAPI();
         getUserMethod.validateResponse();
+    }
+
+    @Test
+    public void verifyRepositories(){
+        GetRepositoriesMethod getRepositoriesMethod = new GetRepositoriesMethod();
+        getRepositoriesMethod.setHeaders(String.format("Authorization=%s", "token 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"));
+        getRepositoriesMethod.callAPI();
+        getRepositoriesMethod.validateResponse();
+    }
+
+    @Test
+    public void verifyCreatedRepository(){
+        CreateRepositoryMethod createRepositoryMethod = new CreateRepositoryMethod();
+        createRepositoryMethod.setHeaders(String.format("Authorization=%s", "token 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"));
+        createRepositoryMethod.addProperty("name", UUID.randomUUID().toString());
+        createRepositoryMethod.callAPI();
+        createRepositoryMethod.validateResponse();
     }
 }
